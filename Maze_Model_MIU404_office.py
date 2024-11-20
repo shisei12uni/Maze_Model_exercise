a57f2559758326ba7390286c657267c7aef6522e
shisei12uni <zcemiui@ucl.ac.uk>
2024-11-20 14:33:44 +0000

    commited readme & the maze exercise from week 3

---
+++ Maze_Model_MIU404_office.py
@@ -0,0 +1,45 @@
+office_for_MIU404 = {
+    "office": {
+     "capacity": 6,
+     "residents": ["Shima", "Ibuki", "Kokonoe"],
+     "directions": {"upstairs" : "kitchen",
+                    "left" : "lockers",
+                    "right" : "toilet"}
+    },
+    
+    "kitchen": {
+     "capacity": 1,
+     "residents" : ["Jimba"],
+     "directions" : {"downstairs": "office",
+                     "jump": "office"}
+    },
+    
+    "toilet": {
+     "capacity": 1,
+     "residents": [],
+     "directions": {"left": "office"},
+    },
+
+    "lockers" :{
+     "capacity": 4,
+     "residents": ["Kikyou"],
+     "directions": {"right": "office"},
+    }
+}
+
+#print (office_for_MIU404.keys()),
+#print (office_for_MIU404.values())
+print ("Mameji" in office_for_MIU404)
+
+people_so_far = 0
+total_capacity = 0
+
+for rooms in office_for_MIU404:
+    people_so_far = people_so_far + len(office_for_MIU404[rooms]["residents"])
+    # print(len(office_for_MIU404[rooms]["residents"])) #check individual rooms
+print(people_so_far)
+
+for rooms in office_for_MIU404:
+    total_capacity = total_capacity + office_for_MIU404[rooms]["capacity"]
+    # print(office_for_MIU404[rooms]["capacity"]) #if it's numbers don't need to use len()
+print(total_capacity)
---
+++ readme.md
@@ -0,0 +1,3 @@
+this is the exercise for week 3 preparation materials.
+It involves both the Maze Model and the Maze Population.
+The commit wasn't saved from the beginning. This is because I didn't understand how Github works, put the exercise .py file with my lecture notes then stashed the lecture notes which ended up with the notes disappearing. I decided to move the .py file to a different folder, which didn't work well with the repositories, and now I'm here trying to sort out what's the correct rocess to git stuff.
